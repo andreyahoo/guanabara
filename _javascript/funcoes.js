283fe8e83ed1281010f9a4de910d56358fd99670
YuriDPaula <yurisamuka@hotmail.com>
2016-10-25 14:15:38 -0200

    primeira vez

---
+++ _javascript/funcoes.js
@@ -0,0 +1,12 @@
+function mudaFoto(foto)
+		 {
+			document.getElementById("icone").src= "_imagens/"+foto+".png";
+		}
+
+
+
+
+// function mudaFoto(foto)
+// 		 {
+// 			document.getElementById("icone").src= foto;
+// 		}
